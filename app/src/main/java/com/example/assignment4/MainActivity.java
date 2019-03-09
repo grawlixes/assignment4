3342bcd16341996df964888865ae30d3c6c5100a
Kyle Franke <kfranke1@binghamton.edu>
2019-03-08 23:59:19 -0500

    User/Password stuff

--- app/src/main/java/com/example/assignment4/MainActivity.java
+++ app/src/main/java/com/example/assignment4/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,7 +44,9 @@ public class MainActivity extends AppCompatActivity
 
         setTitle("Log in");
 
-
+        // Take user input for login.
+        EditText inputUsername = findViewById(R.id.inputUsername);
+        EditText inputPassword = findViewById(R.id.inputPassword);
     }
 
     @Override
